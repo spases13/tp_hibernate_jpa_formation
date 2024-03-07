49162afa64c9c03248ec04d4d233e1ac70d8e03a
Yassine BIZNAOUNE <yassine.biznaoune@e-polytechnique.com>
2024-03-07 11:21:59 +0100

    initial commit

---
+++ spring-hibernate-jpa/src/main/java/fr/norsys/springhibernatejpa/SpringHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package fr.norsys.springhibernatejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateJpaApplication.class, args);
+	}
+
+}
---
+++ spring-hibernate-jpa/src/test/java/fr/norsys/springhibernatejpa/SpringHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.norsys.springhibernatejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
